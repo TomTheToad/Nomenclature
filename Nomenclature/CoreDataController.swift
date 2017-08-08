6ff5d9f322528a13b78fc17eaf5a8e3136b99d55
TomTheToad <tomthetoad951@gmail.com>
2017-08-07 22:18:04 -0400

    Added CoreDataController.

---
+++ Nomenclature/CoreDataController.swift
@@ -0,0 +1,21 @@
+//
+//  CoreDataController.swift
+//  Nomenclature
+//
+//  Created by VICTOR ASSELTA on 8/7/17.
+//  Copyright © 2017 TomTheToad. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CoreDataController {
+    
+    let managedObjectContext: NSManagedObjectContext = {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError("Internal application error")
+        }
+        return appDelegate.persistentContainer.viewContext
+    }()
+    
+}
